824dc42850f9a3a4a8b4accbb05b430d1182c86e
James Knight <contact@mtn-studio.com>
2022-09-25 11:59:53 -0700

    Tweaks to the layout slightly, added in a dark mode toggle but need ot work on the styling of both versions.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+// const toggleDarkMode = document.getElementById('toggleDarkMode');
+
+// toggleDarkMode.addEventListener('click', () => {
+// 	var htmlElement = document.documentElement;
+// 	console.log(`Current theme: ${htmlElement.dataset.theme}`);
+// 	if ((htmlElement.dataset.theme = 'dark')) {
+// 		htmlElement.dataset.theme = 'light';
+// 		console.log(`New theme set to: ${htmlElement.dataset.theme}`);
+// 	} else {
+// 		htmlElement.dataset.theme = 'dark';
+// 	}
+// });
